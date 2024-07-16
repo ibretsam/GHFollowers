a8a548c511586f446f8a12d5232a179d45b9cd28
Khanh Le <masterbruculous@gmail.com>
2024-07-17 00:15:21 +0700

    Passing data between views

---
+++ GHFollowers/FollowerListVC.swift
@@ -0,0 +1,22 @@
+//
+//  FollowerListVC.swift
+//  GHFollowers
+//
+//  Created by MasterBi on 16/7/24.
+//
+
+import UIKit
+
+class FollowerListVC: UIViewController {
+
+    var username: String!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        navigationController?.isNavigationBarHidden = false
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+
+}
