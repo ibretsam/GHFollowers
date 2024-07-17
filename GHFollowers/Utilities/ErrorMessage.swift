de384f18610df64a81a62aa750455068b1fb2472
Khanh Le <masterbruculous@gmail.com>
2024-07-18 01:10:15 +0700

    Refactor error messages

---
+++ GHFollowers/Utilities/ErrorMessage.swift
@@ -0,0 +1,16 @@
+//
+//  ErrorMessage.swift
+//  GHFollowers
+//
+//  Created by MasterBi on 18/7/24.
+//
+
+import Foundation
+
+enum ErrorMessage: String {
+    case invalidUsername = "This username created an invalid request. Please try again."
+    case unableToComplete = "Unable to complete your request. Please check your internet connection."
+    case invalidResponse = "Invalid response from the server. Please try again."
+    case invalidData = "The data received from the server was invalid. Please try again."
+    case authenticationError = "Authentication error. Please check your Github token"
+}
