4bc1c8f3c1d20c81536e46e71e2718d16e0d9968
Khanh Le <masterbruculous@gmail.com>
2024-07-17 23:50:56 +0700

    Making Network call

---
+++ GHFollowers/Managers/NetworkManager.swift
@@ -0,0 +1,56 @@
+//
+//  NetworkManager.swift
+//  GHFollowers
+//
+//  Created by MasterBi on 17/7/24.
+//
+
+import Foundation
+
+class NetworkManager {
+    static let shared = NetworkManager()
+    let baseURL = "https://api.github.com/users/"
+    
+    private init() {}
+    
+    func getFollowers(for username: String, page: Int, completed: @escaping ([Follower]?, String?) -> Void) {
+        let endpoint = baseURL + "\(username)/followers?per_page=100&page\(page)"
+        
+        guard let url = URL(string: endpoint) else {
+            completed(nil, "This username created an invalid request. Please try again.")
+            return
+        }
+        
+        var request = URLRequest(url: url)
+            request.addValue("application/vnd.github+json", forHTTPHeaderField: "Accept")
+            request.addValue("Bearer g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", forHTTPHeaderField: "Authorization")
+            request.addValue("2022-11-28", forHTTPHeaderField: "X-GitHub-Api-Version")
+        
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            if let _ = error {
+                completed(nil, "Unable to complete your request. Please check your internet connection.")
+            }
+            
+            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+                completed(nil, "Invalid response from the server. Please try again.")
+                return
+            }
+            
+            guard let data = data else {
+                completed(nil, "The data received from the server was invalid. Please try again.")
+                return
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                decoder.keyDecodingStrategy = .convertFromSnakeCase
+                let followers = try decoder.decode([Follower].self, from: data)
+                completed(followers, nil)
+            } catch {
+                completed(nil, "The data received from the server was invalid. Please try again.")
+            }
+        }
+        
+        task.resume()
+    }
+}
